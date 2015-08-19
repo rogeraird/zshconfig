e629a288e2b760f5d1fb4defeee1a08781362700
Roger Aird <RogerAird@gmail.com>
2015-08-19 21:04:56 +0100

    link.sh no longer outputs success message upon failure, instead it outputs a useful failure message.

--- link.sh
+++ link.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [ -d "$HOME/.oh-my-zsh" ]; then
-	ln -s $DIR/roger.zsh-theme $HOME/.oh-my-zsh/themes/roger.zsh-theme && mv $HOME/.zshrc $HOME/.zshrc.bak && ln -s $DIR/.zshrc $HOME/.zshrc
+    ln -s $DIR/roger.zsh-theme $HOME/.oh-my-zsh/themes/roger.zsh-theme && mv $HOME/.zshrc $HOME/.zshrc.bak && ln -s $DIR/.zshrc $HOME/.zshrc
+    echo "Now source ~/.zshrc or relog, your old configuration is located at ~/.zshrc.bak"
+else
+    echo "Unable to link .zshrc and roger.zsh-theme. (Have you installed Oh My Zsh?)"
 fi
-echo "Now source ~/.zshrc or relog, your old configuration is located at ~/.zshrc.bak"
+
