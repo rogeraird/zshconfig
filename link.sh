b676d6385d622fa58f6cdfa8c10de6fb5405a4d6
Roger Aird <rogeraird@gmail.com>
2015-09-03 09:49:01 -0400

    Change link.sh to run with zsh. This probably makes more sense anyway, but also allows us to return with an error code when Oh My Zsh is not installed.

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [ -d "$HOME/.oh-my-zsh" ]; then
     ln -s $DIR/roger.zsh-theme $HOME/.oh-my-zsh/themes/roger.zsh-theme && mv $HOME/.zshrc $HOME/.zshrc.bak && ln -s $DIR/.zshrc $HOME/.zshrc
